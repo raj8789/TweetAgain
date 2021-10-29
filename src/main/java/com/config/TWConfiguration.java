8c1b0d4b3365cab5835e933db7db15dca25fe572
raushan raj <raushan.raj@khoros.com>
2021-10-29 18:00:03 +0530

    added configuration() to return Configuration objects in sendtweet & retrieveTweet class

--- src/main/java/com/config/TWConfiguration.java
+++ src/main/java/com/config/TWConfiguration.java
@@ -1,11 +1,18 @@
 package com.config;
 
-import com.fasterxml.jackson.annotation.JsonProperty;
-
 import io.dropwizard.Configuration;
-
-import javax.validation.constraints.NotNull;
+import twitter4j.conf.ConfigurationBuilder;
 
 public class TWConfiguration extends Configuration {
 
+    public static ConfigurationBuilder configurationBuilder()
+    {
+        ConfigurationBuilder cb = new ConfigurationBuilder();
+        cb.setDebugEnabled(true)
+                .setOAuthConsumerKey("Iq2STwqt6DsfrdAX5kZz37RVP")
+                .setOAuthConsumerSecret("zhhpECdqZKhgukHibrQstKDPbXOKxEL6whLcw2fROR0U0TEoQ0")
+                .setOAuthAccessToken("1451106875525636098-LrOSaFuEOKZDGi3QjzLeAPTZQeopZR")
+                .setOAuthAccessTokenSecret("tRtzJ3BnJBJVumiX4BA15xGtwgfyiMTyMZRKCYYDZuOAF");
+        return cb;
+    }
 }
