35b67e9dd312b63c316c6c5b97891201a9738252
raushan raj <raushan.raj@khoros.com>
2021-11-02 12:03:49 +0530

    variable name cb change to configurationBuilder

--- src/main/java/com/config/TWConfiguration.java
+++ src/main/java/com/config/TWConfiguration.java
@@ -7,12 +7,12 @@ public class TWConfiguration extends Configuration {
 
     public static ConfigurationBuilder configurationBuilder()
     {
-        ConfigurationBuilder cb = new ConfigurationBuilder();
-        cb.setDebugEnabled(true)
+        ConfigurationBuilder configurationBuilder = new ConfigurationBuilder();
+        configurationBuilder.setDebugEnabled(true)
                 .setOAuthConsumerKey("Iq2STwqt6DsfrdAX5kZz37RVP")
                 .setOAuthConsumerSecret("zhhpECdqZKhgukHibrQstKDPbXOKxEL6whLcw2fROR0U0TEoQ0")
                 .setOAuthAccessToken("1451106875525636098-LrOSaFuEOKZDGi3QjzLeAPTZQeopZR")
                 .setOAuthAccessTokenSecret("tRtzJ3BnJBJVumiX4BA15xGtwgfyiMTyMZRKCYYDZuOAF");
-        return cb;
+        return configurationBuilder;
     }
 }
